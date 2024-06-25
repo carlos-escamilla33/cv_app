3d61be833228e622257db7bcac36a97f60bfd5b0
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-06-25 16:40:32 -0700

    exporting Output folder

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@
 export {default as FormBuilderContainer} from "./FormBuilderContainer/FormBuilderContainer";
 export {default as Profile} from "./Profile/Profile"
 export {default as ProfileInput} from "./ProfileInput/ProfileInput"
+export {default as Output} from "./Output/Output"
