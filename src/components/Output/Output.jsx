1f9df8859b793fa125a59c60d1726cf3a86538de
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-06-27 22:14:25 -0700

    working on modularizing Profile

--- src/components/Output/Output.jsx
+++ src/components/Output/Output.jsx
@@ -14,10 +14,19 @@ function Output({profile}) {
         <div className="output-container">
             <div className="header">  
                 <h1 className="name">{profile.firstName} {profile.lastName}</h1>
-                <p>{profile.phoneNumber}</p>
-                <p>{profile.email}</p>
+                <p>{profile.phoneNumber} || {profile.email}</p>
             </div>
+            <hr />
+            <div className="tech-skills">
+                <h2>Tech Skills</h2>
+            </div>
+            <hr />
+            <div className="projects">
+                <h2>Projects</h2>
+            </div>
+            <hr />
             <div className="education">
+                <h2>Education</h2>
                 <p>{profile.schoolName}</p>
                 <ul>
                     <li>{<p>{profile.major}</p>}</li>
